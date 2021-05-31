6be30f6ae42f7f60f68b99b055107696d2ab022e
aly1551995 <alymahmoud124@gmail.com>
2021-05-31 23:41:12 +0200

    Added Door.java

---
+++ Door.java
@@ -0,0 +1,10 @@
+import java.awt.Point;
+
+public class Door extends LockableObject{
+
+    public Door(Key key, int id, Point position, int pos_value) {
+        super(key, id, position, pos_value);
+    }
+
+
+}
