078890cbd0ea075821744fcd62c556697acc3c82
aly1551995 <alymahmoud124@gmail.com>
2021-05-31 23:38:43 +0200

    Added AdventureGame.java

---
+++ AdventureGame.java
@@ -0,0 +1,10 @@
+
+
+public class AdventureGame {
+
+    public static void main(String[] args) throws InvalidMove {
+        GameEngine game = new GameEngine();
+        game.initializeGame();
+    }
+}
+
